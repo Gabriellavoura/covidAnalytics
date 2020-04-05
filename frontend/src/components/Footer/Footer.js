b6188634d758ec37f5a72a1d49c7ab878c0019e9
Gabriel Lavoura <gabriellavoura@outlook.com>
2020-04-05 03:27:05 -0300

    Adjusted indentation

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -6,50 +6,49 @@ import { NavItem, NavLink, Nav, Navbar } from "reactstrap";
 
 const Footer = (props) => {
 
-    
-    return (
+  return (
 
-      <div className="navContainer FooterContainer"> 
-        <Navbar light expand="md">          
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink >
-                  © 2020{" "}
-                  <a className="font-weight-bold ml-1 LinkTitle" href="https://www.furg.br/">
-                    iTeCorona
-                  </a>
-              </NavLink>                
-            </NavItem>              
-          </Nav>
-          <Nav >
-              <NavItem>
-                  <NavLink className="LinkTitle font-weight-bold" href="https://www.furg.br/">
-                  Sobre nós
-                  </NavLink>  
-              </NavItem>
-          </Nav>    
-          <Nav >
-            <NavItem>        
-              <NavLink className="LinkTitle font-weight-bold" href="https://www.furg.br/">
-                Portal
-              </NavLink>
-            </NavItem>                  
-          </Nav>
-          <Nav navbar>
-            <NavItem>
-              <NavLink > Powered by: {" "} 
-                <a className="font-weight-bold ml-1 LinkTitle" href="http://www.c3.furg.br/">
-                  Centro de Ciências Computacionais 
+    <div className="navContainer FooterContainer"> 
+      <Navbar light expand="md">          
+        <Nav className="mr-auto" navbar>
+          <NavItem>
+            <NavLink >
+                © 2020{" "}
+                <a className="font-weight-bold ml-1 LinkTitle" href="https://www.furg.br/">
+                  iTeCorona
                 </a>
-                <a href="http://www.c3.furg.br/"> e</a>
-                <a className="font-weight-bold ml-1 LinkTitle"href="http://www.c3.furg.br/">
-                  Furg
-                </a>
-              </NavLink>   
+            </NavLink>                
+          </NavItem>              
+        </Nav>
+        <Nav >
+            <NavItem>
+                <NavLink className="LinkTitle font-weight-bold" href="https://www.furg.br/">
+                Sobre nós
+                </NavLink>  
             </NavItem>
-          </Nav>
-        </Navbar>
-      </div>
-    );
+        </Nav>    
+        <Nav >
+          <NavItem>        
+            <NavLink className="LinkTitle font-weight-bold" href="https://www.furg.br/">
+              Portal
+            </NavLink>
+          </NavItem>                  
+        </Nav>
+        <Nav navbar>
+          <NavItem>
+            <NavLink > Powered by: {" "} 
+              <a className="font-weight-bold ml-1 LinkTitle" href="http://www.c3.furg.br/">
+                Centro de Ciências Computacionais 
+              </a>
+              <a href="http://www.c3.furg.br/"> e</a>
+              <a className="font-weight-bold ml-1 LinkTitle"href="http://www.c3.furg.br/">
+                Furg
+              </a>
+            </NavLink>   
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </div>
+  );
 }
 export default Footer;
